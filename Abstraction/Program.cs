01a975f0db2602821e3f034ac47f130771195a87
Haritha R <harithalaks1998@gmail.com>
2024-06-25 19:41:27 +0530

    Add project files.

---
+++ Abstraction/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creating instances of derived classes
+            Animal dog = new Dog();
+            Animal cat = new Cat();
+
+            // Displaying sounds made by the animals
+            dog.MakeSound();
+            cat.MakeSound();
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Abstraction/abstractClass.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abstraction
+{
+  // Abstract class
+    abstract class Animal
+    {
+ // Abstract method (does not have a body)
+        public abstract void MakeSound();
+    }
+
+  // Derived class (inherits from Animal)
+    class Dog : Animal
+    {
+        // Implementation of abstract method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Dog says: Woof!");
+        }
+    }
+
+    // Derived class (inherits from Animal)
+    class Cat : Animal
+    {
+        // Implementation of abstract method
+        public override void MakeSound()
+        {
+            Console.WriteLine("Cat says: Meow!");
+        }
+    }
+}
+
+
+
